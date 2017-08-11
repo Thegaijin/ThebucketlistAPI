5b497e8712a64a8d06e3c0c98e8f5e512ed1eb3a
Thegaijin <semaganda@outlook.com>
2017-08-11 14:54:36 +0300

    Adding the setup and teardown to tests

---
+++ test_bucketlist.py
@@ -0,0 +1,32 @@
+# test_bucketlist.py
+import unittest
+import os
+import json
+from app import create_app, db
+
+
+class BucketlistTestCase(unittest.TestCase):
+    """This class represents the bucketlist test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.bucketlist = {'name': 'Go to Borabora for vacation'}
+
+        # binds the app to the current context
+        with self.app.app_context():
+            # create all tables
+            db.create_all()
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
