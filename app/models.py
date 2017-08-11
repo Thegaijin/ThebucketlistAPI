bf54bb882a01ac432128a9f1af89a88dd1c8ee12
Thegaijin <semaganda@outlook.com>
2017-08-11 14:08:04 +0300

    Creating the bucketlist table in the DB

---
+++ app/models.py
@@ -0,0 +1,38 @@
+# app/models.py
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login_manager
+
+
+class Bucketlist(db.Model):
+    """This class represents the bucketlist table."""
+
+    __tablename__ = 'bucketlists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    def __init__(self, name):
+        """initialize with name."""
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Bucketlist.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Bucketlist: {}>".format(self.name)
